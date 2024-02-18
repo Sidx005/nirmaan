2b05cd4d6cc4c3ba98edf90ca1cf7de887545dbe
Sidx005 <143771414+Sidx005@users.noreply.github.com>
2024-02-18 07:58:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+let a = 10;
+let b = 5;
+
+// Arithmetic Operators
+console.log('Addition:', a + b);
+console.log('Subtraction:', a - b);
+console.log('Multiplication:', a * b);
+console.log('Division:', a / b);
+console.log('Modulus:', a % b);
+
+
+console.log('Greater than:', a > b);
+console.log('Less than:', a < b);
+console.log('Equal to:', a === b);
+console.log('Not equal to:', a !== b);
+
+
+let x = true;
+let y = false;
+console.log('AND:', x && y);
+console.log('OR:', x || y);
+console.log('NOT:', !x);
